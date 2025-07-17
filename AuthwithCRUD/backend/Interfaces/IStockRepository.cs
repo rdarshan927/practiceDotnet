ba672db64c57cdc9bbde711237617c2cc42c4cf8
rdarshan927 <rdarshan927@gmail.com>
2025-07-17 23:39:08 +0530

    added asynchrnous and linked stock with comment

---
+++ AuthwithCRUD/backend/Interfaces/IStockRepository.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Models;
+using backend.Dtos.Stock;
+
+namespace backend.Interfaces
+{
+    public interface IStockRepository
+    {
+        Task<List<Stock>> GetAllAsync();
+        Task<Stock?> GetByIdAsync(int id);
+        Task<Stock> CreateAsync(Stock stockModel);
+        Task<Stock?> UpdateAsync(int id, UpdateStockRequestDto stockModel);
+        Task<Stock?> DeleteAsync(int id);
+        Task<bool> StockExists(int id);
+    }
+}
