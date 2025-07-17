d244db694b261f08e0a42d15748a65e06fd311a8
rdarshan927 <rdarshan927@gmail.com>
2025-07-17 23:39:34 +0530

    added crud functionalities for comment

--- AuthwithCRUD/backend/Program.cs
+++ AuthwithCRUD/backend/Program.cs
@@ -1,5 +1,8 @@
 using backend.Data;
+using backend.Interfaces;
+using backend.Repositories;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using Pomelo.EntityFrameworkCore.MySql.Infrastructure; // optional but good for some options
 
 
@@ -10,6 +13,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddControllers().AddNewtonsoftJson(options =>
+{
+    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+});
 
 // builder.Services.AddDbContext<ApplicationDBContext>(options =>
 // {
@@ -23,6 +31,8 @@ builder.Services.AddDbContext<ApplicationDBContext>(options =>
     );
 });
 
+builder.Services.AddScoped<IStockRepository, StockRepository>();
+builder.Services.AddScoped<ICommentRepository, CommentRepository>();
 
 var app = builder.Build();
 
---
+++ AuthwithCRUD/backend/Repositories/CommentRepository.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using backend.Data;
+using backend.Interfaces;
+using backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Repositories
+{
+    public class CommentRepository : ICommentRepository
+    {
+        private readonly ApplicationDBContext _context;
+        public CommentRepository(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Comment> CreateAsync(Comment commentModel)
+        {
+            await _context.Comments.AddAsync(commentModel);
+            await _context.SaveChangesAsync();
+            return commentModel;
+        }
+
+        public async Task<Comment?> DeleteAsync(int id)
+        {
+            var existingComment = await _context.Comments.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (existingComment == null)
+            {
+                return null;
+            }
+
+            _context.Comments.Remove(existingComment);
+            await _context.SaveChangesAsync();
+            return existingComment;
+        }
+
+        public async Task<List<Comment>> GetAllAsync()
+        {
+            return await _context.Comments.ToListAsync();
+        }
+
+        public async Task<Comment?> GetByIdAsync(int id)
+        {
+            return await _context.Comments.FindAsync(id);
+        }
+
+        public async Task<Comment?> UpdateAsync(int id, Comment commentModel)
+        {
+            var existingComment = await _context.Comments.FindAsync(id);
+            if (existingComment == null)
+            {
+                return null;
+            }
+
+            existingComment.Title = commentModel.Title;
+            existingComment.Content = commentModel.Content;
+
+            await _context.SaveChangesAsync();
+
+            return existingComment;
+        }
+    }
+}
